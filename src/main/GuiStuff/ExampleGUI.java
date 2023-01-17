667337dfad17ee649166b8fa8e840d736b6a836d
Administrator <sebastian.radimir@live.com>
2023-01-17 10:05:54 +0100

    hier vollbild ding

--- src/main/GuiStuff/ExampleGUI.java
+++ src/main/GuiStuff/ExampleGUI.java
@@ -2,21 +2,27 @@ package GuiStuff;
 
 import javax.swing.*;
 
+import java.awt.*;
+
 import static GuiStuff.Settings.*;
 public class ExampleGUI {
 
     public static void main(String[] args) {
 
         JFrame gameJFame = new JFrame("Game");
-
+        gameJFame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         gameJFame.setResizable(false);
-        gameJFame.setSize(board_width, board_height);
+        gameJFame.setUndecorated(true);
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+
+        board_width = size.width;
+        board_height = size.height;
 
         gameJFame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         gameJFame.setLayout(null);
 
-        gameJFame.add(new Board(3,4));
+        gameJFame.add(new Board(4,4));
 
         gameJFame.setBackground(board_bg_color);
 
--- src/main/GuiStuff/Settings.java
+++ src/main/GuiStuff/Settings.java
@@ -5,8 +5,10 @@ import java.awt.*;
 
 public class Settings {
 
-    public static int board_width = 1280;
-    public static int board_height = 1280;
+    //public static int board_width = 1280;
+    //public static int board_height = 1280;
+    public static int board_width = 1000;
+    public static int board_height = 1000;
     public static Color board_bg_color = new Color(36,70,103);
     public static Color board_line_color = new Color(115, 95, 71);
     public static Color board_tile_color = new Color(206, 171, 96);
