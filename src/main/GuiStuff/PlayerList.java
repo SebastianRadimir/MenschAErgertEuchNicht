a19f77724cc60eeb9733393c46a56dcef05b9794
Denis Maslov <denis.maslov@biotronik.com>
2023-02-14 18:27:27 +0100

    added a function to set the Player in the List to Green

--- src/main/GuiStuff/PlayerList.java
+++ src/main/GuiStuff/PlayerList.java
@@ -5,6 +5,7 @@ import Backend.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.CookieHandler;
 import java.util.ArrayList;
 
 public class PlayerList extends JPanel {
@@ -12,33 +13,49 @@ public class PlayerList extends JPanel {
     //Class Variables-----------------------------------
     private int heightPerPlayer = 100;
     private int widthOfWindow = 200;
-    private Player[] playerlist;
-    private ArrayList<JLabel> playerLabelsList = new ArrayList<>();
+    private final Player[] playerlist;
+    private ArrayList<JPanel> playerPanelList = new ArrayList<>();
+    private Color defaultBackground = Color.WHITE;
     //Constructor---------------------------------------
+
+    /***
+     * initilaze a PlayerList JPanel where every Player is listed
+     * @param playerlist_p
+     * @param heightPerPlayer_p
+     * @param widthOfWindow_p
+     */
     public PlayerList(Player[] playerlist_p, int heightPerPlayer_p,int widthOfWindow_p){
         playerlist = playerlist_p;
         heightPerPlayer = heightPerPlayer_p;
         widthOfWindow = widthOfWindow_p;
         createPanel();
     }
+
+    /***
+     * initilaze a PlayerList JPanel where every Player is listed
+     * @param players_p
+     */
     public PlayerList(Player[] players_p){
         playerlist = players_p;
         createPanel();
     }
     //Functions-----------------------------------------
     private void createPanel(){
-        setLayout(new GridLayout(playerlist.length + 1,2));
+        setLayout(new GridLayout(playerlist.length + 1,1));
         setBackground(Color.gray);
-        setSize(widthOfWindow - 10,heightPerPlayer * playerlist.length + 100);
 
-        JLabel playlerListLabel = new JLabel("Spielerliste");
-        add(playlerListLabel);
-        JLabel figuesHomeLabel = new JLabel("Figuren Zuhause");
-        add(figuesHomeLabel);
+        JPanel topPanel = new JPanel(new GridLayout(1,2));
+        topPanel.add(new JLabel("Spielerliste"));
+        topPanel.add(new JLabel("Figuren Zuhause"));
+        add(topPanel);
 
+        //add for every player a new row with name and maybe stats
         for (Player player:playerlist) {
-            JLabel label = new JLabel(player.getPlayerName());
-            label.setBackground(Color.GREEN);
+            JPanel playerPanel = new JPanel(new GridLayout(1,2));
+            playerPanel.setName(player.getPlayerName());
+
+            JLabel playerLabel = new JLabel(player.getPlayerName());
+            playerPanel.add(playerLabel);
 
             int figureThatAreHome = 0;
             Figure[] figureList = player.getFigures();
@@ -49,23 +66,56 @@ public class PlayerList extends JPanel {
             }
 
             JLabel labelFigureHome = new JLabel(figureThatAreHome + "/" + player.getFigures().length);
-            add(label);
-            add(labelFigureHome);
+            playerPanel.add(labelFigureHome);
+
+            playerPanelList.add(playerPanel);
+            add(playerPanel);
         }
     }
 
-    //manual test main function
+    /**
+     * Sets the whole playerPanel to a green Color and resets every other playerPanel to the defaultColor
+     * @param playerName insert the playername, u can use player.getName()
+     */
+    public void setPlayerToGreen(String playerName){
+        for(JPanel panel:playerPanelList){
+            panel.setBackground(defaultBackground);
+            if(playerName.equals(panel.getName())){
+                panel.setBackground(new Color(0,150,0));
+            }
+        }
+    }
+
+
+    /***
+     * new main function to test manuel the JPanel
+     */
     public static void main (String[] args){
         JFrame frame = new JFrame();
 
         ArrayList<Player> listOfPlayer = new ArrayList<>();
-        for( int i = 1; i <= 6; i++){
-            listOfPlayer.add(new Player(i,5,Color.WHITE,"Player " + i));
+        for( int i = 1; i <= 3; i++){
+            listOfPlayer.add(new Player(i,3,Color.WHITE,"Player " + i));
         }
+        PlayerList test = new PlayerList(listOfPlayer.toArray(new Player[listOfPlayer.size()]));
 
-        frame.add(new PlayerList(listOfPlayer.toArray(new Player[listOfPlayer.size()])));
-        frame.setSize(200,800);
+
+        frame.add(test);
+        frame.setSize(300,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        /*
+        //my own "sleep" thing xD
+        System.out.println("1 green");
+        test.setPlayerToGreen("Player 1");
+        int i = 0;
+        while (i <= 5000000){
+            i = i + 1;
+            System.out.println(i);
+        }
+        System.out.println("2 green");
+        test.setPlayerToGreen("Player 2");
+        */
     }
 }
