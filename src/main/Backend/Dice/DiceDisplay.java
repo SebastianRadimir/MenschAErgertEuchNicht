33c99dbaa410ffe38d0d9f1959d81cf895054f2c
Administrator <sebastian.radimir@live.com>
2023-02-15 09:00:14 +0100

    made the end animation better

--- src/main/Backend/Dice/DiceDisplay.java
+++ src/main/Backend/Dice/DiceDisplay.java
@@ -11,16 +11,21 @@ import java.util.Random;
 public class DiceDisplay extends JPanel {
 
     private int diceVal;
+    private
+    DicePanel selectedDD = null;
+    private boolean toggleDiceColor;
     private JButton acceptButton;
 
     private DicePanel[] dss;
     private double scrollDecelerate;
     private double initSpeed;
     private double offset;
+    private int endTimer, index;
     private Timer t;
     private DiceDialog ddl;
 
     public DiceDisplay(){
+        toggleDiceColor = false;
         setSize(Settings.board_width,Settings.board_height/10);
 
         diceVal = new Random().nextInt(1,7);
@@ -63,12 +68,39 @@ public class DiceDisplay extends JPanel {
 
         t=null;
 
-        int index = ((int)(offset)/this.getHeight())+4;
+        index = ((int)(offset)/this.getHeight())+4;
         if (index<dss.length) {
-            diceVal = dss[index].getPoints();
+            selectedDD = dss[index];
+            diceVal = selectedDD.getPoints();
 
         }
 
+        if (selectedDD != null) {
+            endTimer = 0;
+            t = new Timer(100, new ActionListener() {
+                public void actionPerformed(ActionEvent ae) {
+                    selectAnimation();
+                    repaint();
+                }
+            });
+            t.start();
+        }else {
+            JOptionPane.showMessageDialog(null, "Lets just say you rolled a "+ diceVal+".");
+            closeALL();
+        }
+    }
+
+    private void selectAnimation(){
+        toggleDiceColor = true;
+        if (endTimer>=10){
+            repaint();
+            t.stop();
+            t=null;
+            closeALL();
+        }
+        endTimer++;
+    }
+    private void closeALL(){
         ddl.addWindowListener(new DiceDialog.Closer());
         acceptButton.doClick();
     }
@@ -76,22 +108,37 @@ public class DiceDisplay extends JPanel {
     @Override
     public void paintComponent(Graphics g){
 
-        g.setColor(Settings.board_bg_color);
-        g.fillRect(0,0,this.getWidth(),this.getHeight());
+        if (!toggleDiceColor) {
+            g.setColor(Settings.board_bg_color);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-        int ySize = this.getHeight();
+            int ySize = this.getHeight();
 
-        int i = 0;
-        for (DicePanel dp:dss) {
-            dp.draw(g, (i*ySize)-(int)(offset), 0, ySize, ySize);
-            i++;
+            int i = 0;
+            for (DicePanel dp : dss) {
+                dp.draw(g, (i * ySize) - (int) (offset), 0, ySize, ySize);
+                i++;
+            }
+
+            g.setColor(Settings.dice_selection_color);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(6));
+            g2.drawLine(ySize * 4, 0, ySize * 4, ySize);
+        }else {
+
+            g.setColor(Settings.board_bg_color);
+            int xSize = this.getWidth();
+            int ySize = this.getHeight();
+            g.fillRect(0,0,this.getWidth(),ySize);
+
+            selectedDD.draw(g, (index * ySize) - (int) (offset), 0, ySize, ySize);
+
+            g.setColor(Settings.dice_selection_color);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(6));
+            g2.drawLine(ySize * 4, 0, ySize * 4, ySize);
+
         }
-
-        g.setColor(Settings.dice_selection_color);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(6));
-        g2.drawLine(ySize*4, 0, ySize*4, ySize);
-
     }
 
     public void addDialog(DiceDialog diceDialog) {
