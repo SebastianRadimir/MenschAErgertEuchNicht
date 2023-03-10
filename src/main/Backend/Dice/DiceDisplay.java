fa70c8ccb8af5eefae80d5a1cc0b8f69843e4fe6
sebas <sebastian.radimir@live.com>
2023-03-10 13:59:24 +0100

    centered wining text

--- src/main/Backend/Dice/DiceDisplay.java
+++ src/main/Backend/Dice/DiceDisplay.java
@@ -27,8 +27,10 @@ public class DiceDisplay extends JPanel {
         setSize(Settings.board_width,Settings.board_height/10);
 
         diceVal = new Random().nextInt(1,7);
-        scrollDecelerate = (new Random().nextInt(985,990))/1000.0;
-        initSpeed = new Random().nextInt(100,500);
+        //scrollDecelerate = (new Random().nextInt(985,990))/1000.0;
+        //initSpeed = new Random().nextInt(100,500);
+        scrollDecelerate = 2;
+        initSpeed = 0;
 
         int diceAmount = 8+((int)(initSpeed));
 
@@ -73,7 +75,7 @@ public class DiceDisplay extends JPanel {
 
         if (selectedDD != null) {
             endTimer = 0;
-            t = new Timer(100, ae -> {
+            t = new Timer(1, ae -> { // muss 1 auf 100 gesetzt werden
                 selectAnimation();
                 repaint();
             });
